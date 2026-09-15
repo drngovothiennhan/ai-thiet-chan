6e2eb579013bd273f619aa0c2d62d174d69d5fca
drngovothiennhan <dr.ngovothiennhan@gmail.com>
2026-09-15 09:01:26 +0700

    fix: bust clinical learning cache on production

--- public/consultation.js
+++ public/consultation.js
@@ -1,5 +1,5 @@
 window.__aitcRawFetch=window.__aitcRawFetch||window.fetch.bind(window);
-import('/clinical-learning.js').catch(()=>{});
+import('/clinical-learning.js?v=2.6.1').catch(()=>{});
 
 (()=>{
   const $=id=>document.getElementById(id);
